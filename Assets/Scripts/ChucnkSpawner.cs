ee2cf9f0ab07da8ba818b1b7aa26dc6e29567b39
Alessandro_Cinque <alessandro5cinque@gmail.com>
2021-03-10 20:31:40 +0100

    Finally is working
    In this commit I got the Graph generated and numbered correctly, both in build and debug version with the correct use of prefab. I may just add a slider for the camera just as a cherry on top

--- Assets/Scripts/ChucnkSpawner.cs
+++ Assets/Scripts/ChucnkSpawner.cs
@@ -8,19 +8,18 @@ public class ChucnkSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject AxisPrefab;
     [SerializeField] private Transform myCamera;
-    [SerializeField] private List<TextMeshPro> manyText;
-    
+
     //Just something where to store and modify the current position
     private Vector3 temp;
     //because the pivot point is in the middle of the Axis xSpawn has to be 8 in order to compensate the fact that the pivot point is in the middle
     private float xSpawn = 8;
     // This is like the i in a forloop where you need a variable to iterate, and is also the multiplier for the text
-    private int timesSpawned = 1;
+    private int timesSpawned = 0;
     //This is the offset for spawn the other Axis one after the other
     private int chucnkLength = 16;
     // This is the offset that I use for have the correct numbers on the axis
     private int numOffset;
-    
+
 
     // Update is called once per frame
     void Update()
@@ -30,27 +29,37 @@ public class ChucnkSpawner : MonoBehaviour
         {
             SpawnChucnk();
         }
-        
+
     }
 
     public void SpawnChucnk()
     {
-       
-        
+
+
         temp = transform.position;
         temp.y = -4.5f;
         temp.x = xSpawn;
         //Making sure that the fisrt number of the new axis is correct
         numOffset = timesSpawned * chucnkLength;
+
+        GameObject myPrefab = Instantiate(AxisPrefab, temp, transform.rotation);
+
+
         
-        Instantiate(AxisPrefab, temp, transform.rotation);
-        for (int i = 0; i < manyText.Count; i++)
+        for (int i = 0; i < chucnkLength; i++)
         {
             int my_numbers;
-            //Having the numbers in ascending order
-            my_numbers = (i) + 1 +(numOffset);
-            manyText[i].SetText(my_numbers.ToString());
+            //Having the numbers in ascending order, i+1 because it would start by 0 which I don't want it to.
+            my_numbers = (i) + 1 + (numOffset);
+         
+        
+            TextMeshPro prefabText = myPrefab.transform.GetChild(i).GetComponentInChildren<TextMeshPro>();
+            prefabText.SetText(my_numbers.ToString());
+
         }
+
+
+          
         //Adding the chuckLength as in a for loop so that the next spawn axis is adjacent to the previous one
         xSpawn += chucnkLength;
         //This is part of what makes the number on the axis correct
